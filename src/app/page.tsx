33322c26d37ce9ca089c0dfa01c2002617fff466
Vedant005 <vedantkanekar909@gmail.com>
2025-06-20 14:16:37 +0530

    feat: add blog section with details and links, enhance resume buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ import GitHubCalendar from "react-github-calendar";
 import {
   FaGithub,
   FaLinkedin,
+  FaBookOpen,
+  FaExternalLinkAlt,
 } from "react-icons/fa";
-import { SiX } from "react-icons/si";
-import { SiLeetcode, SiHashnode } from "react-icons/si";
+import { SiLeetcode, SiHashnode, SiX } from "react-icons/si";
+import { CiMail } from "react-icons/ci";
+import { LuCalendar1 } from "react-icons/lu";
+import { IoMdDownload } from "react-icons/io";
 import YearButton from "./utils/YearButton";
 import { getGitHubYears } from "./utils/getYears";
 import { useState } from "react";
@@ -163,11 +167,55 @@ export default function Home() {
     },
   ];
 
-  // type Blog = {
-  //   title: string;
-  //   description: string;
-  //   image: string;
-  // };
+  type Blog = {
+    title: string;
+    description: string;
+    image: string;
+    uploaded: string;
+    duration: string;
+    link: string;
+  };
+
+  const blogs: Blog[] = [
+    {
+      title: "Under the hood of the V8 engine",
+      description:
+        "Dive into the working of Google's V8 engine for Javascript, \n from parsing to the functionality of Turbofan",
+      image:
+        "https://cdn.hashnode.com/res/hashnode/image/upload/v1736741504384/7c50b83b-e70d-4d57-9716-a1c8896201ed.jpeg?auto=compress,format&format=webp",
+      uploaded: "Jan 13, 2025",
+      duration: "6 min read",
+      link: "https://vedants.hashnode.dev/under-the-hood-of-the-v8-engine",
+    },
+    {
+      title: "How Javascript code is executed?",
+      description:
+        "This article will explain about the execution context and the call in a informative way.",
+      image:
+        "https://cdn.hashnode.com/res/hashnode/image/upload/v1736257539976/fad2132b-7d4c-4717-a7bc-8e5c07825e5e.avif?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+      uploaded: "Jan 7, 2025",
+      duration: "4 min read",
+      link: "https://vedants.hashnode.dev/how-javascript-code-is-executed",
+    },
+    {
+      title: "NPM Semantic Versioning",
+      description: "Explains the semantic way of npm versioning.",
+      image: "/npm.webp",
+      uploaded: "Jan 3, 2025",
+      duration: "4 min read",
+      link: "https://vedants.hashnode.dev/npm-semantic-versioning",
+    },
+    {
+      title: "Internal working of require module in Node.js",
+      description:
+        "This article will explain to you what internally happens when you call the node.js require module.",
+      image:
+        "https://cdn.hashnode.com/res/hashnode/image/upload/v1735324727955/90b81d01-02dc-4497-bc8b-86317552e23e.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+      uploaded: "Dec 27,2025",
+      duration: "8 min read",
+      link: "https://vedants.hashnode.dev/internal-working-of-requiring-modules-in-nodejs",
+    },
+  ];
 
   return (
     <main className="min-h-screen bg-[#0f0f0f] text-white  px-4 py-10 ">
@@ -179,9 +227,18 @@ export default function Home() {
               <span className="text-white">I&apos;m Vedant</span>{" "}
               <span className="inline-block animate-waving-hand">👋</span>
             </h1>
+            <p className="text-white mt-1 flex justify-center md:justify-start text-center">
+              <Link
+                href="mailto:vedantkanekar123@gmail.com"
+                className="hover:text-green-300 flex items-center justify-center  gap-2 mb-2 text-md"
+              >
+                <CiMail className="size-5" /> vedantkanekar909@gmail.com
+              </Link>
+            </p>
+
             <p className="text-gray-300 text-lg sm:text-xl max-w-xl  ">
-              Full-Stack Developer with the mindset of building scable backend
-              and responsive frontends with awesome styles. Keen on learning new
+              Full-Stack Developer specialized in building scalable backend and
+              responsive frontends with awesome styles. Keen on learning new
               things and trying to find optimal solution for each software
               problem.
             </p>
@@ -215,8 +272,9 @@ export default function Home() {
           </div>
 
           {/* Image Section */}
-          <div className="w-full md:w-1/2 flex justify-center">
-            <div className="w-100 h-80 rounded-xl overflow-hidden shadow-lg">
+          <div className="w-full md:w-1/2 flex flex-col justify-center ">
+            {/* Profile Image */}
+            <div className="w-full h-80 rounded-xl mb-5 overflow-hidden">
               <Image
                 src="/profile.webp"
                 alt="Vedant Kanekar"
@@ -225,9 +283,32 @@ export default function Home() {
                 className="object-cover w-full h-full"
               />
             </div>
+
+            {/* Resume Buttons */}
+            <div className="flex gap-2 items-center  ">
+              {/* View Resume Button */}
+              <Link
+                href="https://drive.google.com/file/d/1daSSctUpciZmilJZIGYzj-ywk9cFYILp/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-center gap-2 ml-4 px-2 py-2 border border-zinc-300 dark:border-zinc-500 rounded-lg text-sm font-medium hover:border-zinc-400 hover:dark:border-zinc-600 transition w-3/4"
+              >
+                View Resume <FaExternalLinkAlt className="text-xs" />
+              </Link>
+
+              <a
+                href="https://drive.google.com/uc?export=download&id=1daSSctUpciZmilJZIGYzj-ywk9cFYILp"
+                download
+                className="text-emerald-400 hover:text-emerald-300 border border-zinc-300 dark:border-zinc-500 hover:dark:border-zinc-600 rounded-lg p-2 text-xl transition"
+              >
+                <IoMdDownload />
+              </a>
+            </div>
           </div>
         </div>
-        <h1 className="text-2xl text-white mb-4">Contribution Graph</h1>
+        <h1 className="text-3xl font-bold text-white mb-4">
+          Contribution Graph
+        </h1>
         <div className="max-w-4xl flex xl:flex-row flex-col gap-4  ">
           <div className=" border dark:border-zinc-500 border-zinc-200 p-5 max-w-fit max-h-fit rounded-lg  ">
             <GitHubCalendar
@@ -252,7 +333,7 @@ export default function Home() {
         </div>
         <section className="text-white py-16   ">
           <h2 className="text-3xl font-bold mb-6 text-white">Skills</h2>
-          <div className="max-w-6xl mx-auto border dark:border-zinc-500 border-zinc-200  rounded-xl p-5">
+          <div className="max-w-6xl mx-auto border dark:border-zinc-500 rounded-xl p-5">
             <div className="flex flex-wrap gap-5 cursor-pointer">
               {skills.map((skill) => (
                 <div
@@ -271,13 +352,15 @@ export default function Home() {
             </div>
           </div>
         </section>
+
         <section className=" py-10  text-white">
-          <h1 className="text-3xl mb-2">Projects</h1>
+          <h1 className="text-3xl font-bold mb-6 text-white">Projects</h1>
           <div className="max-w-4xl grid md:grid-cols-2 gap-4">
             {projects.map((project, idx) => (
               <div
                 key={idx}
-                className="bg-black border dark:border-zinc-500 border-zinc-200  rounded-xl mb-2 hover:shadow-xl transition"
+                className="bg-black border dark:border-zinc-500 rounded-xl mb-2 h-full
+ "
               >
                 <Image
                   width={400}
@@ -287,46 +370,84 @@ export default function Home() {
                   className="w-full h-50 object-cover  rounded-t-xl"
                 />
                 <div className="p-4 flex flex-col">
-                  <div>
+                  <div className="h-25">
                     <h3 className="text-lg font-semibold">{project.title}</h3>
-                    {/* <p className="text-sm text-gray-400">{project.duration}</p> */}
                     <p className="mt-2 text-gray-300 text-sm">
                       {project.description}
                     </p>
                   </div>
-                  <div className="mt-3 flex flex-wrap gap-2">
-                    {project.tech.map((tech, i) => (
-                      <span
-                        key={i}
-                        className="bg-neutral-800 px-2 py-1 rounded-md text-xs text-white"
-                      >
-                        {tech}
-                      </span>
-                    ))}
+                  <div className="h-15">
+                    <div className="mt-3 mb-3  flex flex-wrap gap-2">
+                      {project.tech.map((tech, i) => (
+                        <div
+                          key={i}
+                          className="bg-neutral-800 px-2 py-1 rounded-md text-xs h-6 text-white"
+                        >
+                          {tech}
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                  <div className="mt-4 flex gap-2">
+                  <div className="p-2 mt-4 flex gap-2">
                     {project.website && (
-                      <a
+                      <Link
                         href={project.website}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="bg-black text-white  hover: border-b-pink-700 text-sm py-1 rounded-md font-medium"
+                        className="bg-white text-black text-sm px-3 py-1 rounded-md font-medium hover:bg-emerald-400"
                       >
                         🌐 Website
-                      </a>
+                      </Link>
                     )}
                     {project.source && (
-                      <a
+                      <Link
                         href={project.source}
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="bg-white text-black text-sm px-3 py-1 rounded-md font-medium hover:bg-gray-200"
+                        className="bg-white text-black text-sm px-3 py-1 rounded-md font-medium hover:bg-emerald-400"
                       >
                         💻 Source
-                      </a>
+                      </Link>
                     )}
                   </div>
                 </div>
+              </div>
+            ))}
+          </div>
+        </section>
+        <section className=" py-10  text-white">
+          <h1 className="text-3xl font-bold mb-6 text-white">Blogs</h1>
+          <div className="max-w-4xl grid md:grid-cols-2 gap-4">
+            {blogs.map((blog, idx) => (
+              <div
+                key={idx}
+                className="bg-black border dark:border-zinc-500 border-zinc-200  rounded-xl mb-2 hover:shadow-xl transition"
+              >
+                <Link href={blog.link} className="block">
+                  <Image
+                    width={400}
+                    height={168}
+                    src={blog.image}
+                    alt={blog.title}
+                    className="w-full h-50 object-cover  rounded-t-xl"
+                  />
+                  <div className="p-4 flex flex-col">
+                    <div>
+                      <h3 className="text-lg font-semibold">{blog.title}</h3>
+                      <p className="text-sm text-gray-400 flex gap-2 items-center">
+                        <FaBookOpen />
+                        {blog.duration}
+                      </p>
+                      <p className="text-sm text-gray-400 flex gap-2 items-center">
+                        <LuCalendar1 />
+                        {blog.uploaded}
+                      </p>
+                      <p className="mt-2 text-gray-300 text-sm">
+                        {blog.description}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
               </div>
             ))}
           </div>
@@ -348,7 +469,7 @@ function LinkWithIcon({
   return (
     <Link
       href={link}
-      className="flex items-center gap-1 px-2 py-1 bg-[#1a1a1a] rounded hover:bg-[#262626] transition"
+      className="flex items-center gap-1 px-2 py-1 bg-[#1a1a1a] rounded hover:bg-[#262626] transition hover:text-emerald-400"
     >
       <span className="text-sm">{icon}</span>
       <span>{label}</span>
